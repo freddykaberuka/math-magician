17eeab21d8f476cdc72ec00c7e3e49da349b32b4
freddykaberuka <freddykaberuka2009@gmail.com>
2022-09-06 16:28:29 -0700

    fix linter

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import reportWebVitals from './reportWebVitals.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App></App>
+    <App>
+    </App>
   </React.StrictMode>,
 );
 
