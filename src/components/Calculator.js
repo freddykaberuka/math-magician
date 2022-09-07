ba72d7ae97406c18ad76660b53b9fa902da35790
freddykaberuka <freddykaberuka2009@gmail.com>
2022-09-07 16:35:01 -0700

    fix feedbacks and fix linter error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,52 +1,56 @@
-/* eslint-disable */
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
-      <div className='main-calculator'>
-          <div className="calculator-wrap">
-              <div className="calculator-result">
-                  0
-              </div>
-              <div className="operation-container">
-              <div className="calculator-upper">
+      <div className="main-calculator">
+        <div className="calculator-wrap">
+          <div className="calculator-result">
+            0
+          </div>
+          <div className="operation-container">
+            <div className="calculator-upper">
               <div className="calculator-number">
-                  <button>AC</button>
-                  <button>+/-</button>
-                  <button>%</button>
+                <button type="button" alt="">AC</button>
+                <button type="button" alt="">+/-</button>
+                <button type="button" alt="">%</button>
               </div>
               <div className="calculator-lower">
-                  <div className="calculator-number">
-                      <button>7</button>
-                      <button>8</button>
-                      <button>9</button>
-                  </div>
-                  <div className="calculator-number">
-                      <button>4</button>
-                      <button>5</button>
-                      <button>6</button>
-                  </div>
-                  <div className="calculator-number">
-                      <button>1</button>
-                      <button>2</button>
-                      <button>3</button>
-                  </div>
-                  <div className="calculator-zero">
-                      <button>0</button>
-                      <button>.</button>
-                  </div>
+                <div className="calculator-number">
+                  <button type="button" alt="">7</button>
+                  <button type="button" alt="">8</button>
+                  <button type="button" alt="">9</button>
+                </div>
+                <div className="calculator-number">
+                  <button type="button" alt="">4</button>
+                  <button type="button" alt="">5</button>
+                  <button type="button" alt="">6</button>
+                </div>
+                <div className="calculator-number">
+                  <button type="button" alt="">1</button>
+                  <button type="button" alt="">2</button>
+                  <button type="button" alt="">3</button>
+                </div>
+                <div className="calculator-zero">
+                  <button type="button" alt="">0</button>
+                  <button type="button" alt="">.</button>
+                </div>
               </div>
-              </div>
-              <div className="calculator-operation">
-                  <button>÷</button>
-                  <button>x</button>
-                  <button>-</button>
-                  <button>+</button>
-                  <button>=</button>
-              </div>
-              </div>
+            </div>
+            <div className="calculator-operation">
+              <button type="button" alt="">÷</button>
+              <button type="button" alt="">x</button>
+              <button type="button" alt="">-</button>
+              <button type="button" alt="">+</button>
+              <button type="button" alt="">=</button>
+            </div>
           </div>
+        </div>
       </div>
     );
   }
