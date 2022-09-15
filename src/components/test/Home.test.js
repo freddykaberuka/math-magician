bce7e6df8cddaf696c1e201b238533576863a77c
ecekpo <ememekpo@yahoo.com>
2022-09-15 14:15:29 +0100

    Add some code on Home.test.js

--- src/components/test/Home.test.js
+++ src/components/test/Home.test.js
@@ -2,6 +2,7 @@ import renderer from 'react-test-renderer';
 import { HashRouter as Router } from 'react-router-dom';
 import Home from '../../pages/Home';
 
+// Test for rendering the navigation
 describe('Test for rendering the navigation', () => {
   it('It should render home page', () => {
     const home = renderer
