1e8f499f43914fa269ccdb65ebfc9f032ccd36e2
freddykaberuka <freddykaberuka2009@gmail.com>
2022-09-16 00:40:40 -0700

    Add new test

--- src/components/test/Calculate.test.js
+++ src/components/test/Calculate.test.js
@@ -1,6 +1,6 @@
 import calculate from '../../logic/calculate';
 
-test('It should return calculaions', () => {
+test('It should return the addition of calculation', () => {
   const obj = {
     total: 6,
     next: 3,
@@ -11,5 +11,57 @@ test('It should return calculaions', () => {
     next: null,
     operation: null,
     total: '9',
+  });
+});
+test('It should return the substraction of calculation', () => {
+  const obj = {
+    total: 6,
+    next: 3,
+    operation: '-',
+  };
+  const buttonName = '=';
+  expect(calculate(obj, buttonName)).toEqual({
+    next: null,
+    operation: null,
+    total: '3',
+  });
+});
+test('It should return the division of calculation', () => {
+  const obj = {
+    total: 6,
+    next: 3,
+    operation: '÷',
+  };
+  const buttonName = '=';
+  expect(calculate(obj, buttonName)).toEqual({
+    next: null,
+    operation: null,
+    total: '2',
+  });
+});
+test('It should return the multiplication of calculation', () => {
+  const obj = {
+    total: 6,
+    next: 3,
+    operation: 'x',
+  };
+  const buttonName = '=';
+  expect(calculate(obj, buttonName)).toEqual({
+    next: null,
+    operation: null,
+    total: '18',
+  });
+});
+test('It should return the modulas of calculation', () => {
+  const obj = {
+    total: 6,
+    next: 3,
+    operation: '%',
+  };
+  const buttonName = '=';
+  expect(calculate(obj, buttonName)).toEqual({
+    next: null,
+    operation: null,
+    total: '0',
   });
 });
